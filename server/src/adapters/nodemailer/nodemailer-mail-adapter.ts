5e85b3f4aabc8168fbd312d7848d9b9f4da82959
vinimed988 <vinimed.98@gmail.com>
2022-05-06 11:13:49 -0300

    Implementation of SOLID

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "ad5aa9cf1ca28c",
+        pass: "20a54f9c3edc5e"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Vinícius Medeiros <vinimed.98@gmail.com>',
+            subject,
+            html: body,
+        });
+    }
+}
